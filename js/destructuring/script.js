703a16bc49c0db8ee0ecf074504427fc03f62c66
Ashon Shakya <shakya.ashon@gmail.com>
2025-10-27 12:00:00 +1100

    quick-tips update

---
+++ js/destructuring/script.js
@@ -0,0 +1,12 @@
+const car = { make: "Toyota", model: "RAV4", year: 2020 };
+
+// const make = car.make
+// const model = car.model
+// const year = car.year
+
+// Object destructuring
+const { make, model, year } = car;
+
+console.log(make); // Toyota
+console.log(model); // RAV4
+console.log(year); // 2020
