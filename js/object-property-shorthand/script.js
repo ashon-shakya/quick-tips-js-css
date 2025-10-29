57039fc2fab5cc5823fc04f99e4882c06e5f17bd
Ashon Shakya <shakya.ashon@gmail.com>
2025-10-29 12:00:00 +1100

    quick-tips update

---
+++ js/object-property-shorthand/script.js
@@ -0,0 +1,13 @@
+const make = "Toyota";
+const model = "RAV4";
+const year = 2020;
+
+// individually provide object property
+const car1 = {
+  make: make,
+  model: model,
+  year: year,
+};
+
+// object property shorthand
+const car2 = { make, model, year };
