c9bdcea3deef3527cf84cd470d6ed46108c7386d
Ashon Shakya <shakya.ashon@gmail.com>
2025-10-30 12:00:00 +1100

    quick-tips update

---
+++ js/short-circuit-evaluation/script.js
@@ -0,0 +1,9 @@
+const isLoggedIn = true;
+
+// using if
+if (isLoggedIn) {
+  console.log("Welcome back!");
+}
+
+// using && for short circuit evaluation
+isLoggedIn && console.log("Welcome back!");
