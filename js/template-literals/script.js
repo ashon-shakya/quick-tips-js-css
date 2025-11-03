e74b83d2867f0f06c531d03a8429ada331a25cbf
Ashon Shakya <shakya.ashon@gmail.com>
2025-11-03 12:00:00 +1100

    quick-tips update

---
+++ js/template-literals/script.js
@@ -0,0 +1,19 @@
+const firstName = "Prem";
+const lastName = "Acharya";
+const address = "Sydney";
+
+// string concatenation using + operator
+console.log(
+  "Hi! my name is " +
+    firstName +
+    " " +
+    lastName +
+    " and I live in " +
+    address +
+    "."
+);
+
+// string concatenation using template literals
+console.log(
+  `Hi! my name is ${firstName} ${lastName} and I live in ${address}.`
+);
